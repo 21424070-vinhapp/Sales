3f57b101c081f6b488d37424008afc0e7fcb3bd3
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-06-15 17:03:49 +0700

    update code

--- app/src/main/java/com/example/sales/presentations/main/MainActivity.java
+++ app/src/main/java/com/example/sales/presentations/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.sales;
+package com.example.sales.presentations.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.sales.R;
 
 public class MainActivity extends AppCompatActivity {
 
