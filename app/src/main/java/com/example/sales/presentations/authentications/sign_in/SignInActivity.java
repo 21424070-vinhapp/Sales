c986a7ada1d0c618456a6417fd8c5619a6aa5932
Quang Vinh <quangvinhkhongcovo@gmail.com>
2022-06-16 14:08:41 +0700

    finish setup UI

--- app/src/main/java/com/example/sales/presentations/authentications/sign_in/SignInActivity.java
+++ app/src/main/java/com/example/sales/presentations/authentications/sign_in/SignInActivity.java
@@ -1,31 +1,73 @@
 package com.example.sales.presentations.authentications.sign_in;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.os.Bundle;
-
-import com.example.sales.R;
+import com.example.sales.data.datasource.remote.AppResource;
+import com.example.sales.data.datasource.remote.Respone.UserRespone;
+import com.example.sales.databinding.ActivitySignInBinding;
+import com.example.sales.ultils.AppConstant;
 
 public class SignInActivity extends AppCompatActivity {
     SignInViewModel signInViewModel;
-
+    ActivitySignInBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        mBinding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
-        signInViewModel=new ViewModelProvider(this, new ViewModelProvider.Factory() {
+        addViewModel();
+
+        addEvent();
+
+        addComformStatus();
+
+
+        signInViewModel.performLogIn("demo100@gmail.com", "123456789");
+    }
+
+    private void addEvent() {
+
+    }
+
+    private void addViewModel() {
+        signInViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
             @NonNull
             @Override
             public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
                 return (T) new SignInViewModel(getApplicationContext());
             }
         }).get(SignInViewModel.class);
+    }
 
-        signInViewModel.performLogIn("demo100@gmail.com","123456789");
+    private void addComformStatus() {
+        signInViewModel.getUserStatus().observe(this, new Observer<AppResource<UserRespone>>() {
+            @Override
+            public void onChanged(AppResource<UserRespone> userResponeAppResource) {
+                switch (userResponeAppResource.status)
+                {
+                    case LOADING:
+                        Log.d(AppConstant.TAG, "onChanged: LOADING" );
+                        mBinding.layoutLoading.layoutLoading.setVisibility(View.VISIBLE);
+                        break;
+                    case SUCCESS:
+                        Log.d(AppConstant.TAG, "onChanged: SUCCESS" );
+                        mBinding.layoutLoading.layoutLoading.setVisibility(View.GONE);
+                        break;
+                    case ERROR:
+                        Log.d(AppConstant.TAG, "onChanged: ERROR" + userResponeAppResource.message);
+                        break;
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sales/ultils/AppConstant.java
+++ app/src/main/java/com/example/sales/ultils/AppConstant.java
@@ -2,4 +2,5 @@ package com.example.sales.ultils;
 
 public class AppConstant {
     public static String BASE_URL = "https://appsale.herokuapp.com/";
+    public static String TAG="BBB";
 }
